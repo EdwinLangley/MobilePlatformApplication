c2af38160ef7d1cef45ee534b26a9e4a1204cc8e
Edwin J Langley <edlangley@live.co.uk>
2018-10-07 17:30:03 +0100

    Initial commit for the module

---
+++ app/src/main/java/com/example/edwin/neighbourhooddiary/Splash.java
@@ -0,0 +1,54 @@
+package com.example.edwin.neighbourhooddiary;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_splash);
+
+        img = (ImageView) findViewById(R.id.imageView);
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+        rotateAnimation.setDuration(400);
+        rotateAnimation.setRepeatCount(0);
+        img.startAnimation(rotateAnimation);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this, WelcomeScreen.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
---
+++ app/src/main/java/com/example/edwin/neighbourhooddiary/WelcomeScreen.java
@@ -0,0 +1,25 @@
+package com.example.edwin.neighbourhooddiary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+
+    public void openMapPage(View view){
+        Intent mapIntent = new Intent(this, MapsActivity.class);
+        startActivity(mapIntent);
+    }
+
+
+}
