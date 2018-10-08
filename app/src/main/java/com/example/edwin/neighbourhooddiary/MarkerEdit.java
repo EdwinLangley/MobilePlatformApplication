3bdc73eabba1b94e232c97a6ffda07b4ff656796
Edwin J Langley <edlangley@live.co.uk>
2018-10-08 21:27:31 +0100

    Better start screen - doesn't look like a toy phone now

--- app/src/main/java/com/example/edwin/neighbourhooddiary/MarkerEdit.java
+++ app/src/main/java/com/example/edwin/neighbourhooddiary/MarkerEdit.java
@@ -2,13 +2,17 @@ package com.example.edwin.neighbourhooddiary;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -17,6 +21,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MarkerEdit extends AppCompatActivity {
 
     private Spinner spinner;
+    private ImageView markerIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,8 @@ public class MarkerEdit extends AppCompatActivity {
 
         final EditText title = (EditText) findViewById(R.id.title);
         Button button = (Button) findViewById(R.id.save);
+        markerIcon = (ImageView) findViewById(R.id.markerIcon);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
@@ -45,12 +52,44 @@ public class MarkerEdit extends AppCompatActivity {
                 finish();
             }
         });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selection = spinner.getSelectedItem().toString();
+
+
+                switch (selection) {
+                    case "Post":   markerIcon.setImageResource(R.drawable.post);
+                        break;
+                    case "ATM":  markerIcon.setImageResource(R.drawable.atm);
+                        break;
+                    case "Public WC":  markerIcon.setImageResource(R.drawable.wc);
+                        break;
+                    case "Parking":   markerIcon.setImageResource(R.drawable.parking);
+                        break;
+                    case "Car Charging":  markerIcon.setImageResource(R.drawable.carcharge);
+                        break;
+                    case "General Attractions":  markerIcon.setImageResource(R.drawable.attraction);
+                        break;
+                    case "Picnic Areas":  markerIcon.setImageResource(R.drawable.picnic);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+
+        });
     }
 
 
     public void loadSpinner(){
         String[] arraySpinner = new String[] {
-                "1", "2", "3", "4", "5"
+                "PostBox", "ATM", "Public WC", "Parking", "Car Charging", "Picnic Areas", "General Attractions"
         };
 
         spinner = (Spinner) findViewById(R.id.spinner);
