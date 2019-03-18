75a183d5af12febfed35087d7c6fd4e52824b5ec
Edwin Langley <edwinlangley@edwins-mbp.home>
2019-03-18 19:22:44 +0000

    Start and End markers have now been added and the application zooms to the right area

--- app/src/main/java/com/example/edwin/neighbourhooddiary/displayTracking.java
+++ app/src/main/java/com/example/edwin/neighbourhooddiary/displayTracking.java
@@ -1,14 +1,23 @@
 package com.example.edwin.neighbourhooddiary;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
@@ -46,7 +55,6 @@ public class displayTracking extends FragmentActivity implements OnMapReadyCallb
         allUsers = new ArrayList<>();
 
 
-
         mMarkerReference = FirebaseDatabase.getInstance().getReference().child("markers");
         uReference = FirebaseDatabase.getInstance().getReference().child("users");
 
@@ -73,25 +81,25 @@ public class displayTracking extends FragmentActivity implements OnMapReadyCallb
     }
 
 
-    public void LoadInLocations(DataSnapshot dataSnapshot){
-            allUsers.clear();
-            for(DataSnapshot ds : dataSnapshot.getChildren() ){
-                User user = ds.getValue(User.class);
+    public void LoadInLocations(DataSnapshot dataSnapshot) {
+        allUsers.clear();
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+            User user = ds.getValue(User.class);
 
-                allUsers.add(user);
+            allUsers.add(user);
 
-            }
         }
+    }
 
-    public void locateCurrentUser(){
+    public void locateCurrentUser() {
 
         thisUser = new User();
 
-        for(User u : allUsers){
+        for (User u : allUsers) {
             String uName = u.getDisplayName();
             String aName = acct.getDisplayName();
 
-            if(uName.matches(aName)){
+            if (uName.matches(aName)) {
                 thisUser = u;
             }
         }
@@ -99,19 +107,19 @@ public class displayTracking extends FragmentActivity implements OnMapReadyCallb
         splitIntoLongAndLat();
     }
 
-    public void splitIntoLongAndLat(){
+    public void splitIntoLongAndLat() {
 
         ArrayList<Double> longs = new ArrayList<>();
         ArrayList<Double> lats = new ArrayList<>();
 
-        Map<String,String> latlngMap = thisUser.getGpsLocations();
+        Map<String, String> latlngMap = thisUser.getGpsLocations();
 
         ArrayList<String> latlngString = new ArrayList<String>(latlngMap.values());
 
-        for(String s : latlngString){
+        for (String s : latlngString) {
 
-            String snew = s.replace(",",".");
-            snew = snew.replace("lat","");
+            String snew = s.replace(",", ".");
+            snew = snew.replace("lat", "");
 
             String[] splits = snew.split("lng");
 
@@ -122,19 +130,29 @@ public class displayTracking extends FragmentActivity implements OnMapReadyCallb
             lats.add(Double.parseDouble(LatStr));
         }
 
-        mapPoints(longs,lats);
+        mapPoints(longs, lats);
 
     }
 
-    public void mapPoints(ArrayList<Double> longs, ArrayList<Double> lats){
+    public void mapPoints(ArrayList<Double> longs, ArrayList<Double> lats) {
 
         ArrayList<LatLng> points = new ArrayList<LatLng>();
         PolylineOptions polyLineOptions = new PolylineOptions();
 
-        for(int i = 0; i < lats.size() - 1; i++){
-            points.add(new LatLng(lats.get(i),longs.get(i)));
-            points.add(new LatLng(lats.get(i+1),longs.get(i+1)));
+        for (int i = 0; i < lats.size() - 1; i++) {
+            points.add(new LatLng(lats.get(i), longs.get(i)));
+            points.add(new LatLng(lats.get(i + 1), longs.get(i + 1)));
         }
+
+        double startLatitude = points.get(0).latitude;
+        double startLongitude = points.get(0).longitude;
+        double endLatitude = points.get(points.size() - 1).latitude;
+        double endLongitude = points.get(points.size() - 1).longitude;
+        LatLng start = new LatLng(startLatitude, startLongitude);
+        LatLng end = new LatLng(endLatitude, endLongitude);
+
+        mMap.addMarker(new MarkerOptions().position(start).title("start"));
+        mMap.addMarker(new MarkerOptions().position(end).title("end"));
 
         polyLineOptions.width(7 * 1);
         polyLineOptions.geodesic(true);
@@ -160,9 +178,33 @@ public class displayTracking extends FragmentActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        Criteria criteria = new Criteria();
+
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        if (location != null)
+        {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(17)                   // Sets the zoom
+                    .bearing(0)                // Sets the orientation of the camera to east
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
     }
 }
